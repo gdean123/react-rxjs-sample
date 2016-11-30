81e7f1982e0a6ac37feb26d0fbffe9b6b7ff4c3b
Eric Tsiliacos and George Dean <pair+etsiliacos+gdean@pivotal.io>
2016-11-29 17:52:38 -0800

    Create a counter using RxJS
    - We are not taking advantage of React's virtual dom to avoid rerendering unchanged nodes, which is unfortunate

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Rx from 'rxjs'
 
-const App = () => <p> Hello React!</p>;
+const incrementStream = new Rx.BehaviorSubject();
+const decrementStream = new Rx.BehaviorSubject();
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+incrementStream.next(1);
+decrementStream.next(-1);
+
+const Counter = ({total, incrementStream, decrementStream}) => (
+    <div>
+        <button onClick={() => incrementStream.next(1) }>Increment</button>
+        <button onClick={() => decrementStream.next(-1) }>Decrement</button>
+        <p>{total}</p>
+    </div>
+
+);
+
+const App = ({total, incrementStream, decrementStream}) => (
+    <Counter total={total}
+             incrementStream={incrementStream}
+             decrementStream={decrementStream}/>
+);
+
+Rx.Observable.merge(incrementStream, decrementStream)
+    .scan((accumulator, currentValue) => accumulator + currentValue, 0)
+    .subscribe(total => {
+        ReactDOM.render(<App {...{total, incrementStream, decrementStream}}/>, document.getElementById('app'));
+    });

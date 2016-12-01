4c959304e27095f07cc33a96ca706443867b8698
Eric Tsiliacos and George Dean <pair+etsiliacos+gdean@pivotal.io>
2016-12-01 13:50:44 -0800

    Start using react component state
    - This will now work with React's DOM diffing
    - Subscribe to and unsubscribe from streams in lifecycle methods
    - Stop rendering in a subscribe block.

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -2,29 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Rx from 'rxjs'
 
-const incrementStream = new Rx.BehaviorSubject();
-const decrementStream = new Rx.BehaviorSubject();
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
 
-incrementStream.next(1);
-decrementStream.next(-1);
+        this.incrementStream = new Rx.BehaviorSubject();
+        this.decrementStream = new Rx.BehaviorSubject();
 
-const Counter = ({total, incrementStream, decrementStream}) => (
-    <div>
-        <button onClick={() => incrementStream.next(1) }>Increment</button>
-        <button onClick={() => decrementStream.next(-1) }>Decrement</button>
-        <p>{total}</p>
-    </div>
+        this.state = {total: 0};
+    }
 
+    componentDidMount() {
+        this.incrementStream.next(1);
+        this.decrementStream.next(-1);
+
+        this.subscription = Rx.Observable.merge(this.incrementStream, this.decrementStream)
+            .scan((accumulator, currentValue) => accumulator + currentValue, 0)
+            .subscribe(total => {
+                this.setState({total})
+            });
+    }
+
+    componentWillUnmount() {
+        this.subscription.unsubscribe();
+    }
+
+    render() {
+        const {total} = this.state;
+
+        return (
+            <div>
+                <button onClick={() => this.incrementStream.next(1) }>Increment</button>
+                <button onClick={() => this.decrementStream.next(-1) }>Decrement</button>
+                <p>{total}</p>
+            </div>
+        )
+    }
+}
+
+const App = () => (
+    <Counter/>
 );
 
-const App = ({total, incrementStream, decrementStream}) => (
-    <Counter total={total}
-             incrementStream={incrementStream}
-             decrementStream={decrementStream}/>
-);
-
-Rx.Observable.merge(incrementStream, decrementStream)
-    .scan((accumulator, currentValue) => accumulator + currentValue, 0)
-    .subscribe(total => {
-        ReactDOM.render(<App {...{total, incrementStream, decrementStream}}/>, document.getElementById('app'));
-    });
+ReactDOM.render(<App/>, document.getElementById('app'));
